8581766f4018530ac4109fd01e111d17c2e1e40b
Akash Susil <81065844+agentsuperuser@users.noreply.github.com>
2023-10-15 19:31:58 +0530

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -32,7 +32,7 @@ const Work = () => {
 <div class="bg-black/40 max-w-sm backdrop-blur-2xl rounded-xl p-2 overflow-hidden grid items-center text-2xl text-white/50">
   <img class="w-full rounded-xl" src={Ancillary} alt="Sunset in the mountains"/>
   <div class="px-4 pt-4 pb-2 gap-y-3">
-  <div class="font-bold text-xl text-white mb-2">Main Food Counter</div>
+  <div class="font-bold text-xl text-white mb-2">Ancillary's Counter</div>
   <div className=''>
     <span class="inline-block bg-[#e12ef831] text-base border-2 font-bold border-emerald-400 rounded-full px-3 py-1 text-white mt-2 mr-2">Helado Fritta</span>
     <span class="inline-block bg-[#e12ef831] text-base border-2 font-bold border-emerald-400 rounded-full px-3 py-1 text-white mt-2 mr-2">Bloody Sweet</span>
