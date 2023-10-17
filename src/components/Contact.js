2f022cea58521ced393320ff86e83799b0c5206f
Akash Susil <81065844+agentsuperuser@users.noreply.github.com>
2023-10-17 22:24:14 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
             <br/>Nedumparambu P O, Nagaroor, Kerala 695102
           </p>
           <span class="inline-flex">
-            <a class="ml-4 text-gray-500">
+            <a href="https://instagram.com/euphoria_rihmct" class="ml-4 text-gray-500">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
